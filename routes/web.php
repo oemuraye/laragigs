b68078b7a2081b6d20d1be94b83b53eee836cad5
oemuraye <oemuraye@gmail.com>
2023-06-07 11:06:58 +0100

    added template

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,24 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/listings/{id}', function ($id) {
+// Route::get('/listings/{id}', function ($id) {
+//     $listing = Listing::find($id);
+
+//     if ($listing) {
+//         return view('listing', [
+//             'heading' => 'Listing Details',
+//             'listing' => $listing
+//         ]);
+//     } else {
+//         abort('404');
+//     }
+
+// });
+//alternatively
+
+Route::get('/listings/{listing}', function (Listing $listing) {
     return view('listing', [
         'heading' => 'Listing Details',
-        'listing' => Listing::find($id)
+        'listing' => $listing
     ]);
 });

2f0a4a3cc8400dc7c79089c103ba4e0455ea0d5e
oemuraye <oemuraye@gmail.com>
2024-01-15 17:27:50 +0100

    first deployment

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -42,7 +42,7 @@
         @if($listing->user_id == auth()->id())
             <x-card class="mt-4 p-2 flex space-x-6">
                 <a href="/listings/{{$listing->id}}/edit">
-                    <i class="fa-solid fa-pencil"></i> Edit Gig
+                    <i class="fa-solid fa-pencil"></i> Edit
                 </a>
                 <form method="POST" action="/listings/{{$listing->id}}">
                     @csrf

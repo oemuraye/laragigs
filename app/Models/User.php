3f3cdc8c9e5a872d48ddb4d8cee2e86c28b165a9
oemuraye <oemuraye@gmail.com>
2024-01-18 06:14:18 +0100

    added filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -46,5 +47,10 @@ class User extends Authenticatable
     // Relationship With Listings
     public function listings() {
         return $this->hasMany(Listing::class, 'user_id');
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, 'admin@example.com') && $this->hasVerifiedEmail();
     }
 }
